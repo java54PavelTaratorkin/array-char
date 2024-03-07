285d205a9493d8cf699ed479557e37f35a3ac7da
PaulT1983 <pault83@gmail.com>
2024-03-07 19:14:02 +0200

    ArrayChar compareTo test

---
+++ src/telran/arrays/ArrayChar.java
@@ -0,0 +1,14 @@
+package telran.arrays;
+
+public class ArrayChar {
+	private char[] array;
+	
+	public ArrayChar(char[] array) {
+		this.array = array;
+	}
+	
+	public int compareTo(ArrayChar another) {
+		//TODO
+		return -1;
+	}
+}
